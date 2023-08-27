0bb228d33631691be693e2a9e19a18a2019d9bf1
Ilya Zorin <by.92@mail.ru>
2023-08-27 21:31:15 +0500

    Обновляю свойства у UILabel
    Добавил свойства у текстового лейбла, добавил констрейнт ширины. Теперь если текста много, он переносится на строчки ниже

--- LessonThree/Controllers/LabelController.swift
+++ LessonThree/Controllers/LabelController.swift
@@ -24,23 +24,27 @@ class LabelController: UIViewController {
             
             view.addSubview(label)
             label.translatesAutoresizingMaskIntoConstraints = false
+            label.numberOfLines = 0
+            label.textAlignment = .center
+            
             
             NSLayoutConstraint.activate([
                 label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+                label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                label.widthAnchor.constraint(equalTo: view.widthAnchor)
             ])
-    
-                    let attributedText = NSMutableAttributedString(string: "Hello ", attributes: [
-                    .font: UIFont(name: "HelveticaNeue-Bold", size: 50)!
-                    ])
-                    
-                    let greenText = NSAttributedString(string: "world", attributes: [
-                        .font: UIFont(name: "HelveticaNeue-Bold", size: 50)!,
-                        .foregroundColor: UIColor.green
-                    ])
-                    
-                    attributedText.append(greenText)
-                    label.attributedText = attributedText
+            
+            let attributedText = NSMutableAttributedString(string: "Hello ", attributes: [
+                .font: UIFont(name: "HelveticaNeue-Bold", size: 50)!
+            ])
+            
+            let greenText = NSAttributedString(string: "world", attributes: [
+                .font: UIFont(name: "HelveticaNeue-Bold", size: 50)!,
+                .foregroundColor: UIColor.green
+            ])
+            
+            attributedText.append(greenText)
+            label.attributedText = attributedText
         }
         
     }
