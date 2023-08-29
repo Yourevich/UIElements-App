4c357b7878f5b0317d5f3b39997fb78b4d7d2833
Ilya Zorin <by.92@mail.ru>
2023-08-29 08:56:17 +0500

    Добавляю анимацию для UISlider
    Добавил новый констрейнт по оси Y, теперь сам слайдер меняет свое положение по мере движения ползунка

--- LessonThree/Controllers/SliderController.swift
+++ LessonThree/Controllers/SliderController.swift
@@ -4,21 +4,19 @@
 //
 //  Created by Илья Зорин on 22.08.2023.
 //
-
 import UIKit
 
 class SliderController: UIViewController {
     
-    
     let slider = UISlider(frame: CGRect(x: 40, y: 200, width: 300, height: 100))
-    
+    var centerYAnchor: NSLayoutConstraint?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupSlider()
+        slider.addTarget(self, action: #selector(updPointOfSlide), for: .valueChanged)
     }
-    
     
     func setupSlider() {
            view.addSubview(slider)
@@ -35,7 +33,28 @@ class SliderController: UIViewController {
    
     
     
+        view.addSubview(slider)
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        
+        centerYAnchor = slider.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        
+        NSLayoutConstraint.activate([
+            slider.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            centerYAnchor!,
+            slider.widthAnchor.constraint(equalToConstant: 300),
+            slider.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
     
-
-
+    @objc func updPointOfSlide() {
+        let sliderValue = CGFloat(slider.value)
+        let maxY = view.frame.height - slider.frame.height / 2
+        let minY = slider.frame.height / 2
+        let newY = minY + sliderValue * (maxY - minY)
+        
+        centerYAnchor?.constant = newY
+    }
 }
+
+
+
